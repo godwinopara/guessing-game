988c4153a5f6a79811b32e22c0fa15bf1b8c1dbd
Godwin Opara <godwinopara62@yahoo.com>
2021-07-20 20:28:04 +0400

    created the getter method to get the number of attempts the user have made in the guessing game

--- guessing_game_project/lib/guessing_game.rb
+++ guessing_game_project/lib/guessing_game.rb
@@ -1,3 +1,11 @@
 class GuessingGame
+    def initialize(min, max)
+        @secret_num = rand(min..max)
+        @num_attempts = 0
+        @game_over = false
+    end
 
+    def num_attempts
+        @num_attempts
+    end
 end
